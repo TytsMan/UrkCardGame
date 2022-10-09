aa69f379691ca24bc1711d29f10379dffb07f5d9
Ivan Dekhtiarov <JamesDeht@gmail.com>
2022-10-09 10:54:57 +0300

    Update CardCanvasView.swift

--- UrkCardGame/Components/CardCanvasView.swift
+++ UrkCardGame/Components/CardCanvasView.swift
@@ -15,21 +15,23 @@ protocol ContainerView: View {
 struct CardCanvasView<Content: View>: ContainerView {
     var content: () -> Content
     
+    let cornerRadius: CGFloat = 30.0
+    
     var body: some View {
         VStack(content: content)
             .padding(30)
             .background(Assets.Colors.secondaryColor.swiftUIColor)
-            .cornerRadius(30)
+            .cornerRadius(cornerRadius)
             .background {
                 ZStack {
                     Assets.Colors.secondaryColor.swiftUIColor
                         .opacity(0.3)
-                        .cornerRadius(30)
+                        .cornerRadius(cornerRadius)
                         .scaleEffect(x: 0.98)
                         .offset(y: 15)
                     Assets.Colors.secondaryColor.swiftUIColor
                         .opacity(0.15)
-                        .cornerRadius(30)
+                        .cornerRadius(cornerRadius)
                         .scaleEffect(x: 0.95)
                         .offset(y: 30)
                 }
